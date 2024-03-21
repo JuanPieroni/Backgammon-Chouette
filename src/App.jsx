9576478ce67fdde9a11cac35b7da96a57408b9af
JuanPieroni <113479467+JuanPieroni@users.noreply.github.com>
2024-03-21 16:44:12 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ // App.js
+import React from 'react';
+import GameBoard from './GameBoard';
+ 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Backgammon Chouette</h1>
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
+
